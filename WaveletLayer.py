7d5607ab252145901606caed2b9ca23b7d092abf
Divya Dadlani <divya.dadlani@gmail.com>
2016-03-03 15:23:36 -0500

    Working on a wavelet implementation for the neural net

--- WaveletLayer.py
+++ WaveletLayer.py
@@ -1,9 +1,15 @@
 from pybrain.structure.modules.neuronlayer import NeuronLayer
+from pybrain.structure.parametercontainer import ParameterContainer
+import scipy.signal as spsig
 
 
-class WaveletLayer(NeuronLayer):
+class WaveletLayer(NeuronLayer, ParameterContainer):
+    def __init__(self):
+        ParameterContainer.__init__(self, 1)
+
     def _forwardImplementation(self, inbuf, outbuf):
-        outbuf[:] = inbuf
+        outbuf[:] = spsig.ricker(inbuf, self.params)
 
     def _backwardImplementation(self, outerr, inerr, outbuf, inbuf):
-        inerr[:] = outerr
+        # rickerderiv =
+        inerr[:] = outbuf - outerr
--- nn.py
+++ nn.py
@@ -3,6 +3,7 @@ import pybrain.datasets as pd
 from pybrain.supervised.trainers import BackpropTrainer
 import numpy as np
 from util import *
+from WaveletLayer import *
 
 
 def nn_setup():
@@ -10,26 +11,26 @@ def nn_setup():
     net = ps.FeedForwardNetwork()
 
     inputLayer = ps.LinearLayer(68)
-    hiddenLayer1 = ps.SigmoidLayer(10)
-    hiddenLayer2 = ps.SigmoidLayer(34)
-    hiddenLayer3 = ps.SigmoidLayer(10)
+    hiddenLayer1 = WaveletLayer(10)
+    # hiddenLayer2 = ps.SigmoidLayer(20)
+    # hiddenLayer3 = ps.SigmoidLayer(10)
     outputLayer = ps.SigmoidLayer(2)
 
     input_to_hidden = ps.FullConnection(inputLayer, hiddenLayer1)
-    hidden1_to_hidden2 = ps.FullConnection(hiddenLayer1, hiddenLayer2)
-    hidden2_to_hidden3 = ps.FullConnection(hiddenLayer2, hiddenLayer3)
-    hidden3_to_output = ps.FullConnection(hiddenLayer2, outputLayer)
+    # hidden1_to_hidden2 = ps.FullConnection(hiddenLayer1, hiddenLayer2)
+    # hidden2_to_hidden3 = ps.FullConnection(hiddenLayer2, hiddenLayer3)
+    hidden3_to_output = ps.FullConnection(hiddenLayer1, outputLayer)
 
     # Add input, hidden and output layers and connections
     net.addInputModule(inputLayer)
     net.addModule(hiddenLayer1)
-    net.addModule(hiddenLayer2)
-    net.addModule(hiddenLayer3)
+    # net.addModule(hiddenLayer2)
+    # net.addModule(hiddenLayer3)
     net.addOutputModule(outputLayer)
 
     net.addConnection(input_to_hidden)
-    net.addConnection(hidden1_to_hidden2)
-    net.addConnection(hidden2_to_hidden3)
+    # net.addConnection(hidden1_to_hidden2)
+    # net.addConnection(hidden2_to_hidden3)
     net.addConnection(hidden3_to_output)
 
     # Initialize NN
@@ -38,30 +39,21 @@ def nn_setup():
 
 
 def main():
-    data_source = "../shared_dir/kaggle_data/clips/Patient_1/"
+    data_source = "../clips/Patient_1/"
 
-    ictal_data = np.zeros((30, 68, 500))
-    interictal_data = np.zeros((30, 68, 500))
+    ictal_data = np.zeros((70, 68, 500))
+    interictal_data = np.zeros((104, 68, 500))
 
     print "Loading data files from files at %s" % data_source
     ictal_files, interictal_files, test_files = load_for_patient(data_source)
-    print np.array(ictal_files).shape
-    print np.array(interictal_files).shape
-    for index in range(0, len(ictal_files)-1):
-        # print len(interictal_files)
-        print index
+    for index in range(0, len(ictal_files)):
         ictal_data[index], ictal_freq, ictal_channels, ictal_latency = get_contents_at_index(ictal_files, index)
-    for index in range(0, len(interictal_files)-1):
-        # print len(interictal_files)
-        print index
+    for index in range(0, len(ictal_files)):
         interictal_data[index], interictal_freq, interictal_channels, interictal_latency = get_contents_at_index(
             interictal_files, index)
 
-    ictal = np.split(ictal_data, 3)
-    interictal = np.split(interictal_data, 3)
-
-    train_data = np.reshape(np.vstack([ictal[0], ictal[1], interictal[0], interictal[1]]), (20000, 68))
-    valid_data = np.reshape(np.vstack([ictal[2], interictal[2]]), (10000, 68))
+    train_data = np.reshape(np.vstack([ictal_data[:50, :, :], interictal_data[:50, :, :]]), (50000, 68))
+    valid_data = np.reshape(np.vstack([ictal_data[50:, :, :], interictal_data[50:70, :, :]]), (20000, 68))
 
     ntrain = train_data.shape[0]
     nvalid = valid_data.shape[0]
@@ -85,9 +77,9 @@ def main():
     trainer = BackpropTrainer(net, train_dataset)
     epochs = 0
     train_error = 1.0
-    while epochs < 20:
+    while epochs < 10:
         train_error = trainer.train()
-        print "Epoch %d, Training error: %f" % epochs % train_error
+        print "Epoch %d, Training error: %f" % (epochs, train_error)
         epochs += 1
 
     # print "Training error: %f" % train_error
@@ -95,7 +87,7 @@ def main():
     print "Generating validation dataset"
 
     for index in range(nvalid):
-        if index < ntrain / 2:
+        if index < nvalid / 2:
             valid_dataset.addSample(valid_data[index], is_ictal)
         else:
             valid_dataset.addSample(valid_data[index], is_interictal)
@@ -112,11 +104,10 @@ def main():
 
         else:
             classif[i] = 0
-    labels = np.append(np.ones(5000), np.zeros(5000))
+    labels = np.append(np.ones(10000), np.zeros(10000))
 
-    valid_error = (nvalid - sum(np.equal(classif, labels)))/float(nvalid)
+    valid_error = (nvalid - sum(np.equal(classif, labels))) / float(nvalid)
     print "Validation error: %f" % valid_error
-
 
 if __name__ == '__main__':
     main()
