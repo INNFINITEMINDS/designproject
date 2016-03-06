2e5fb4d483c5b86f274ca434ccccd276aac01cae
Divya Dadlani <divya.dadlani@gmail.com>
2016-03-06 18:36:32 -0500

    Neural network works with 58 % accuracy but cannot do more than 2 patients

--- nn.py
+++ nn.py
@@ -12,23 +12,23 @@ def nn_setup():
     # Create NN
     net = ps.FeedForwardNetwork()
 
-    inputLayer = ps.LinearLayer(50)
-    hiddenLayer1 = ps.SigmoidLayer(10)
-    hiddenLayer2 = ps.SigmoidLayer(20)
-    hiddenLayer3 = ps.SigmoidLayer(10)
-    outputLayer = ps.SigmoidLayer(1)
+    input_layer = ps.LinearLayer(50)
+    hidden_layer1 = ps.SigmoidLayer(40)
+    hidden_layer2 = ps.SigmoidLayer(20)
+    hidden_layer3 = ps.SigmoidLayer(10)
+    output_layer = ps.SigmoidLayer(1)
 
-    input_to_hidden = ps.FullConnection(inputLayer, hiddenLayer1)
-    hidden1_to_hidden2 = ps.FullConnection(hiddenLayer1, hiddenLayer2)
-    hidden2_to_hidden3 = ps.FullConnection(hiddenLayer2, hiddenLayer3)
-    hidden3_to_output = ps.FullConnection(hiddenLayer1, outputLayer)
+    input_to_hidden = ps.FullConnection(input_layer, hidden_layer1)
+    hidden1_to_hidden2 = ps.FullConnection(hidden_layer1, hidden_layer2)
+    hidden2_to_hidden3 = ps.FullConnection(hidden_layer2, hidden_layer3)
+    hidden3_to_output = ps.FullConnection(hidden_layer3, output_layer)
 
     # Add input, hidden and output layers and connections
-    net.addInputModule(inputLayer)
-    net.addModule(hiddenLayer1)
-    net.addModule(hiddenLayer2)
-    net.addModule(hiddenLayer3)
-    net.addOutputModule(outputLayer)
+    net.addInputModule(input_layer)
+    net.addModule(hidden_layer1)
+    net.addModule(hidden_layer2)
+    net.addModule(hidden_layer3)
+    net.addOutputModule(output_layer)
 
     net.addConnection(input_to_hidden)
     net.addConnection(hidden1_to_hidden2)
@@ -46,7 +46,7 @@ def main():
     # num_channels = 68
     # num_samples = 500
 
-    print "Loading data files from files at %s" % data_source
+    print "Preparing to load data from %s" % data_source
     # ictal_files, interictal_files = load_freq_bands_for_patient(data_source)
     patient_data = load_all_freq_bands(data_source)
 
@@ -56,42 +56,37 @@ def main():
     valid_labels = []
 
     for patient in patient_data:
-        num_channels = patient[0]
-        num_samples = patient[1]
-        ictal_files = patient[2]
-        interictal_files = patient[3]
+        num_samples = int(patient[0])
+        ictal_files = patient[1]
+        interictal_files = patient[2]
 
         # Number of ictal OR interictal files. Total files will be num_files*2
         num_files = min(len(ictal_files), len(interictal_files))
 
-        ictal_data = np.zeros((num_files, num_channels, num_samples))
-        interictal_data = np.zeros((num_files, num_channels, num_samples))
+        ictal_data = []
+        interictal_data = []
 
         for index in range(0, num_files):
-            ictal_data[index] = ictal_files[index].get('filtered_data')
+            ictal_data.extend(ictal_files[index].get('filtered_data'))
         for index in range(0, num_files):
-            interictal_data[index] = interictal_files[index].get('filtered_data')
+            interictal_data.extend(interictal_files[index].get('filtered_data'))
 
-        dim = (num_samples / 50) * num_channels * num_files
+        num_channels = len(ictal_data) / 50
 
-        ictal_data = np.reshape(ictal_data, (dim, 50))
-        interictal_data = np.reshape(interictal_data, (dim, 50))
-        # num_valid_files = int(round(num_files / 3))
-        # num_train_files = num_files - num_valid_files
-        num_valid = int(round(num_files / 3))
+        ictal_data = np.reshape(ictal_data, (-1, 50))
+        interictal_data = np.reshape(interictal_data, (-1, 50))
+        num_valid = int(round(num_files / 4))
         num_train = num_files - num_valid
 
         train_data.extend(np.vstack([ictal_data[:num_train], interictal_data[:num_train]]))
-        train_labels.extend(np.vstack([np.ones(num_train), np.zeros(num_train)]))
+
+        train_labels.extend(np.ones(num_train))
+        train_labels.extend(np.zeros(num_train))
+
         valid_data.extend(np.vstack([ictal_data[num_train:num_files], interictal_data[num_train:num_files]]))
-        valid_labels.extend(np.vstack([np.ones(num_valid), np.zeros(num_valid)]))
-        # train_data.extend(
-        #     np.reshape(np.vstack([ictal_data[:num_train_files, :, :], interictal_data[:num_train_files, :, :]]),
-        #                (num_train_files * num_channels * dim * 2, 50)))
-        # valid_data.extend(np.reshape(
-        #     np.vstack([ictal_data[num_train_files:num_train_files + num_valid_files:, :, :],
-        #                interictal_data[num_train_files:num_train_files + num_valid_files, :, :]]),
-        #     (num_valid_files * num_channels * dim * 2, 50)))
+
+        valid_labels.extend(np.ones(num_valid))
+        valid_labels.extend(np.zeros(num_valid))
 
     nvalid = len(valid_data)
     ntrain = len(train_data)
@@ -108,13 +103,10 @@ def main():
     print("Training network on given data")
     trainer = BackpropTrainer(net, train_dataset)
     epochs = 0
-    train_error = 1.0
-    while epochs < 1:
+    while epochs < 200:
         train_error = trainer.train()
         print "Epoch %d, Training error: %f" % (epochs, train_error)
         epochs += 1
-
-    # print "Training error: %f" % train_error
 
     print "Generating validation dataset"
 
@@ -122,22 +114,12 @@ def main():
         valid_dataset.addSample(valid_data[index], valid_labels[index])
 
     print "Classifying validation data"
-    # output = np.round(np.concatenate(net.activateOnDataset(valid_dataset)))
-    output = net.activateOnDataset(valid_dataset)
-    # num = len(output)
-    # classif = np.zeros(nvalid)
-    # for i in range(num):
-    #     # print i
-    #     if output[i, 0] > output[i, 1]:
-    #         classif[i] = 1
-    #
-    #     else:
-    #         classif[i] = 0
-    # labels = np.append(np.ones(nvalid / 2), np.zeros(nvalid - (nvalid / 2)))
+    output = np.round(np.concatenate(net.activateOnDataset(valid_dataset)))
 
     valid_error = (nvalid - sum(np.equal(output, valid_labels))) / float(nvalid)
     print "Validation error: %f" % valid_error
-
+    correct_pred = (1 - valid_error) * 100
+    print "Correct predictions : %.2f%%" % correct_pred
 
 if __name__ == '__main__':
     main()
