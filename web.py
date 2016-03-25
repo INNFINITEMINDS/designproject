2e0e2b502ad101b7da74a9be2783b1a998784c90
Richa  Rajgolikar <rajgolik@p148.ecf.utoronto.ca>
2016-03-25 17:45:09 -0400

    integration of webpage with NN - WIP

--- web.py
+++ web.py
@@ -9,18 +9,20 @@ isSeizure = False
 # Define a function for the thread
 def print_is_seizure( threadName, delay):
 	count = 0
-	while count < 20:
+	#loop to poll value of isSeizure every ~2 seconds and call network
+	while True:		
 		time.sleep(delay)
-		count += 1
+		
 		if isSeizure:
-			print "%s: %s : seizure" % ( threadName, time.ctime(time.time()) )
+			main("yes")
+			# print "%s: %s : seizure" % ( threadName, time.ctime(time.time()) )
 		else:
-			print "%s: %s : not seizure" % ( threadName, time.ctime(time.time()) )
+			main("no")
+			# print "%s: %s : not seizure" % ( threadName, time.ctime(time.time()) )
 
 
 @route('/')
-def root():
-	
+def root():	
 	# return 'Welcome, go to /seizure/yes or /seizure/no to start this thing'
 	return template("main.html")
 
@@ -29,22 +31,20 @@ def root():
 def hello(arg):
 	global isSeizure
 	if arg == 'no':
-		isSeizure = False
-		# return 'Nothing to see here'
-		return template("main_interictal.html")
+		isSeizure = False		
+		return template("main_interictal.html")		
 
 	else:
 		isSeizure = True
-		# return 'ITS HAPPENING !!!'
 		return template("main_ictal.html")
 
-test_util.main()
 
 
 if __name__ == "__main__":
 	# 
 	isSeizure = False
-	thread.start_new_thread( print_is_seizure, ("Thread-1", 3, ) )
+	split_data()
+	thread.start_new_thread( print_is_seizure, ("Thread-1", 0.5, ) )
 	run(host='localhost', port=8082)
 	# print_time( "threadName", 5)
 
