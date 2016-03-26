d0ebafd34bec7b57acf3f43433c7c2714c12dbc4
Richa  Rajgolikar <rajgolik@remote.ecf.utoronto.ca>
2016-03-25 22:25:13 -0400

    tried synchronization bet user toggling buttons - WIP

--- web.py
+++ web.py
@@ -1,10 +1,11 @@
 import thread
 import time
 from bottle import route, run, template
-from test_util import *
+from test_util_ import *
 
 
 isSeizure = False
+switch = False
 
 # Define a function for the thread
 def print_is_seizure( threadName, delay):
@@ -12,13 +13,21 @@ def print_is_seizure( threadName, delay):
 	#loop to poll value of isSeizure every ~2 seconds and call network
 	while True:		
 		time.sleep(delay)
-		
-		if isSeizure:
-			main("yes")
-			# print "%s: %s : seizure" % ( threadName, time.ctime(time.time()) )
-		else:
-			main("no")
-			# print "%s: %s : not seizure" % ( threadName, time.ctime(time.time()) )
+
+		# need some sort of synchronization mechanism here bet the thread in print_is_seizure
+		# and thread in test_my_nn. wait() and signal() ??
+		if switch:
+			thread.exit()
+
+		if isSeizure:			
+			print "%s: %s : seizure" % ( threadName, time.ctime(time.time()) )
+			test_my_nn("yes")
+		else:			
+			print "%s: %s : not seizure" % ( threadName, time.ctime(time.time()) )
+			test_my_nn("no")
+
+		# thread.exit()
+		# thread.start_new_thread( print_is_seizure, ("Thread-1", 0.5, ) )
 
 
 @route('/')
@@ -29,22 +38,26 @@ def root():
 
 @route('/seizure/:arg')
 def hello(arg):
-	global isSeizure
+	global isSeizure, switch
+
 	if arg == 'no':
-		isSeizure = False		
+		isSeizure = False
+		switch = True
 		return template("main_interictal.html")		
 
 	else:
-		isSeizure = True
+		isSeizure = True	
+		switch = True	
 		return template("main_ictal.html")
 
 
 
 if __name__ == "__main__":
 	# 
-	isSeizure = False
-	split_data()
+	isSeizure = False	
 	thread.start_new_thread( print_is_seizure, ("Thread-1", 0.5, ) )
+	main()
+	print 'Spliced data'
 	run(host='localhost', port=8082)
 	# print_time( "threadName", 5)
 
