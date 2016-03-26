2d380edf9d204436e0206d622c804c73e6daa8e5
Richa  Rajgolikar <rajgolik@remote.ecf.utoronto.ca>
2016-03-25 22:26:38 -0400

    Mod code to continue looping over data and not terminate after 115000 s total length

--- test_util_.py
+++ test_util_.py
@@ -13,7 +13,7 @@ interictal_data = []
 interictal_labels = []
 
 #split data before testing network
-def split_data():
+def main():
     testfile = open(testfile_path, 'rb+')
 
     testdict = pickle.load(testfile)
@@ -21,39 +21,69 @@ def split_data():
     inp_labels = testdict['test_labels']
 
     data_index = len(inp_data) / 2
+    global ictal_data
     ictal_data = inp_data[:data_index]
+    global interictal_data
     interictal_data = inp_data[data_index:]
 
     label_index = len(inp_data) / 2
+    global ictal_labels
     ictal_labels = inp_labels[:label_index]
+    global interictal_labels
     interictal_labels = inp_labels[label_index:]
 
+    return ictal_data, ictal_labels, interictal_data, interictal_labels
 
-def main(arg):
+
+def test_my_nn(arg):
         
     net = NetworkReader.readFrom(netfile_path)
 
+    index = 0
     ictal_index = 0
     interictal_index = 0
+    test_data = []
+    test_labels = []
+    global ictal_data, ictal_labels, interictal_data, interictal_labels
 
     #loop to send 1 second (500 datapoints) worth of data to network
     #continues to send until user toggles button
+    # need to change while condition to while thread.isAlive()
     while True:
-        if(arg == 'yes'):
-            print 'User-ictal'
-            length = 1 * 500
-            test_error, true_pos, true_neg, num_seizures = test_nn(net, ictal_data[ictal_index:ictal_index+length], ictal_labels[ictal_index:ictal_index+length])
-            ictal_index += length
+        if(arg == "yes"):            
+            if(ictal_index >= 110500):
+                ictal_index = 0
+                index = 0
+                ictal_data = []
+                ictal_labels = []
 
-        elif(arg == "no"):
-            print 'User-inter ictal'            
             length = 1 * 500
-            test_error, true_pos, true_neg, num_seizures = test_nn(net, interictal_data[interictal_index:interictal_index+length], interictal_labels[interictal_index:interictal_index+length])
+            test_data.append(ictal_data[ictal_index:ictal_index+length])
+            test_labels.append(ictal_labels[ictal_index:ictal_index+length])
+            test_error, true_pos, true_neg, num_seizures = test_nn(net, test_data[index],test_labels[index])
+            ictal_index += length
+            index += 1
+            # print "INDEX: " 
+            # print ictal_index , index
+
+        elif(arg == "no"):           
+            if(interictal_index >= 110500):
+                interictal_index = 0
+                index = 0
+                interictal_data = []
+                interictal_labels = []
+
+            length = 1 * 500
+            test_data.append(interictal_data[interictal_index:interictal_index+length])
+            test_labels.append(interictal_labels[interictal_index:interictal_index+length])
+            test_error, true_pos, true_neg, num_seizures = test_nn(net, test_data[index], test_labels[index])
             interictal_index +=  length
-            
+            index += 1
+            # print "INDEX: "
+            # print interictal_index , index
+    
         print "Test error: %02f, True pos: %02f, True neg: %02f" % (test_error, true_pos, true_neg)
     # print "Number of seizures detected: %d, Actual number of seizures: %d" % (num_seizures, test_num_seizures[i])
-    # raw_input("Press enter for next case")
 
 
 if __name__ == '__main__':
